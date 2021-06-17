49a899a5f9f5106c1c458600603f0b2a20fff5ab
Andrei <livanovandrej99@gmail.com>
2021-06-17 21:33:26 +0700

    Add likes

---
+++ src/main/java/com/example/sitwic/domain/util/MessageHelper.java
@@ -0,0 +1,9 @@
+package com.example.sitwic.domain.util;
+
+import com.example.sitwic.domain.User;
+
+public abstract class MessageHelper {
+    public static String getAuthorName(User author) {
+        return author != null ? author.getUsername() : "<none>";
+    }
+}
---
+++ src/main/java/com/example/sitwic/service/MessageService.java
@@ -0,0 +1,31 @@
+package com.example.sitwic.service;
+
+import com.example.sitwic.domain.User;
+import com.example.sitwic.domain.dto.MessageDto;
+import com.example.sitwic.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+    private MessageRepo messageRepo;
+
+    @Autowired
+    public void setMessageRepo(MessageRepo messageRepo) {
+        this.messageRepo = messageRepo;
+    }
+
+    public Page<MessageDto> messageList(Pageable pageable, String filter, User user) {
+        if (filter != null && !filter.isEmpty()) {
+            return messageRepo.findByTag(filter, pageable, user);
+        } else {
+            return messageRepo.findAll(pageable, user);
+        }
+    }
+
+    public Page<MessageDto> messageListForUser(Pageable pageable, User currentUser, User author) {
+        return messageRepo.findByUser(pageable, author, currentUser);
+    }
+}
